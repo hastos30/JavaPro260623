d278556afb1dbec0c65b76fecf0edd07aa87eb0a
Viktor Shapoval <hastos30@gmail.com>
2023-07-04 15:48:43 +0300

    Created a class of employees with data about them

---
+++ JavaPro260623/src/main/java/lesson2/Employees.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class Employees {
+    private String firstName;
+    private String lastName;
+    private String jobTitle;
+    private String eMail;
+    private String numberPhone;
+    private int age;
+
+}
