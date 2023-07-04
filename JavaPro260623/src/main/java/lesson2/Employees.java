855a2ec7fea959f1d8d2861d00bed27e00e39006
Viktor Shapoval <hastos30@gmail.com>
2023-07-04 16:06:26 +0300

    created сonstructor and geter\seter for class Employees

--- JavaPro260623/src/main/java/lesson2/Employees.java
+++ JavaPro260623/src/main/java/lesson2/Employees.java
@@ -8,4 +8,60 @@ public class Employees {
     private String numberPhone;
     private int age;
 
+    public Employees(String firstName, String lastName, String jobTitle, String eMail, String numberPhone, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.jobTitle = jobTitle;
+        this.eMail = eMail;
+        this.numberPhone = numberPhone;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getNumberPhone() {
+        return numberPhone;
+    }
+
+    public void setNumberPhone(String numberPhone) {
+        this.numberPhone = numberPhone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
