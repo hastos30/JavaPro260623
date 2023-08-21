0f5b5490147327e6bdd87f36a22e9e035570eadb
Viktor Shapoval <hastos30@gmail.com>
2023-08-21 18:02:01 +0300

    Implemented a telephone directory

---
+++ JavaPro260623/src/main/java/lesson9/task2/phonebook/Record.java
@@ -0,0 +1,24 @@
+package main.java.lesson9.task2.phonebook;
+
+public class Record {
+    private String name;
+    private String phoneNumber;
+
+    public Record(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name + ", Телефон: " + phoneNumber;
+    }
+}
